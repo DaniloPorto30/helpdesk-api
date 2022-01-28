7a6d2d483f5bd385af93e1e505cab554d1bf8dc7
Danilo Porto <55693717+DaniloPorto30@users.noreply.github.com>
2022-01-28 14:27:27 -0300

    Update TecnicoController.java

--- src/main/java/com/danilo/Help_app/controller/TecnicoController.java
+++ src/main/java/com/danilo/Help_app/controller/TecnicoController.java
@@ -18,7 +18,7 @@ import com.danilo.Help_app.service.TecnicoService;
 
 
 @RestController
-@CrossOrigin(origins = "https://dreamy-jang-746cbf.netlify.app")
+@CrossOrigin(origins = "https://helpdesk-app.netlify.app")
 @RequestMapping("/tec")
 public class TecnicoController {
 
