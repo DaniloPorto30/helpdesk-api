56a0a0634fc415c7ee302fd4ae973eda63abab76
Danilo Porto <55693717+DaniloPorto30@users.noreply.github.com>
2022-01-28 14:24:29 -0300

    Update LoginController.java

--- src/main/java/com/danilo/Help_app/controller/LoginController.java
+++ src/main/java/com/danilo/Help_app/controller/LoginController.java
@@ -20,7 +20,7 @@ import com.danilo.Help_app.service.LoginService;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://dreamy-jang-746cbf.netlify.app")
 @RequestMapping("/log")
 public class LoginController {
 
